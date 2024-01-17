9b915f8563f8a0677c2888b7d03ed5eb8d52b391
Ayush Jha <96854429+Ayushjha132@users.noreply.github.com>
2024-01-17 20:17:06 +0530

    inital screen with jet setup

---
+++ settings.py
@@ -0,0 +1,11 @@
+
+# contains all the settings of game
+class Settings:
+    
+    def __init__(self):
+
+        self.screen_width = 1280
+        self.screen_height = 700 
+        self.bg_color = (230, 230, 230) 
+        self.ship_speed = 1.5
+
